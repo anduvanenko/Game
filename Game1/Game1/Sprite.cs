04d58ae999c3b614893abe7340c3eb45295cc2b2
anduvanenko <anduvanenko@gmail.com>
2018-01-31 21:56:19 -0500

    Add files via upload

---
+++ Game1/Game1/Sprite.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1
+{
+    class Sprite
+    {
+        //Asset name for the sprite's texture
+        public string AssetName;
+
+        //size of the sprite (with scale applied)
+        public Rectangle size;
+
+        //amount to increate/decrease the size of the original sprite
+        private float scale = 0.4f;
+
+        //current position of sprite
+        public Vector2 Position = new Vector2(50, 50);
+
+        //texture object used when drawing the sprite
+        private Texture2D mSpriteTexture;
+
+        public void LoadContent(ContentManager theContentManager, string theAssetName)
+        {
+            mSpriteTexture = theContentManager.Load<Texture2D>(theAssetName);
+            AssetName = theAssetName;
+            size = new Rectangle(0, 0, (int)(mSpriteTexture.Width * Scale), (int)(mSpriteTexture.Height * Scale));
+        }
+
+        //Draw the sprite to the screen
+        public void Draw(SpriteBatch theSpriteBatch)
+        {
+            theSpriteBatch.Draw(mSpriteTexture, Position,
+                new Rectangle(0, 0, mSpriteTexture.Width, mSpriteTexture.Height),
+                Color.White, 0.0f, Vector2.Zero, Scale, SpriteEffects.None, 0);
+        }
+
+        public float Scale
+        {
+            get { return scale; }
+            set
+            {
+                scale = value;
+                //Recalcualte the size of the sprite with the new scale
+                size = new Rectangle(0, 0, (int)(mSpriteTexture.Width * Scale), (int)(mSpriteTexture.Height * Scale));
+            }
+        }
+
+        public void Update(GameTime theGameTime, Vector2 theSpeed, Vector2 theDirection)
+        {
+            Position += theDirection * theSpeed * (float)theGameTime.ElapsedGameTime.TotalSeconds;
+        }
+    }
+}
